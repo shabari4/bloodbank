66cb5a6c1b3629ec5fb5c3a17833f112eb5367b3
shabari nath <pendyalashabari@yahoo.com>
2018-03-21 17:08:50 +0530

    Initial commit

---
+++ app/src/main/java/com/example/shabarinath/bloodbank/MainActivity.java
@@ -0,0 +1,96 @@
+
+package com.example.shabarinath.bloodbank;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity
+{
+    EditText memail,mpassword;
+    Button login;
+
+    private FirebaseAuth mauth;
+    private FirebaseAuth.AuthStateListener mautlistener;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        memail=(EditText) findViewById(R.id.email);
+        mpassword=(EditText) findViewById(R.id.password);
+        login=(Button) findViewById(R.id.login);
+        mauth=FirebaseAuth.getInstance();
+        mautlistener=new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth)
+            {
+                if(firebaseAuth.getCurrentUser()!=null)
+                {
+//
+                }
+
+            }
+        };
+
+        login.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                signin();
+
+
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mauth.addAuthStateListener(mautlistener);
+    }
+
+    public void signin()
+    {
+        String email=memail.getText().toString();
+        String password=mpassword.getText().toString();
+
+        if(TextUtils.isEmpty(email)||TextUtils.isEmpty(password))
+        {
+            Toast.makeText(MainActivity.this,"Fields are empty",Toast.LENGTH_SHORT).show();
+
+        }
+        else
+        {
+            mauth.signInWithEmailAndPassword(email,password).addOnCompleteListener(MainActivity.this ,new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(!task.isSuccessful())
+                    {
+                        Toast.makeText(MainActivity.this,"Sign in problem",Toast.LENGTH_SHORT).show();
+
+                    }
+                    else {
+                        Intent intent = new Intent(MainActivity.this,successfull.class);
+                        startActivity(intent);
+
+                    }
+                }
+            });
+        }
+    }
+}
---
+++ app/src/main/java/com/example/shabarinath/bloodbank/successfull.java
@@ -0,0 +1,13 @@
+package com.example.shabarinath.bloodbank;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class successfull extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_successfull);
+    }
+}
